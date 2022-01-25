26c8cf7879091432d088af1ca07dbbe8d2c0055f
CJMcClure <cjmcclure1337@gmail.com>
2022-01-25 10:57:50 -0600

    CORS

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require("cors");
 
 
 //middlewares
+app.use(cors())
 app.use(express.json());
+
 
 //routers
 const currencyRouter= require("./routes/currencyRouter.js")
